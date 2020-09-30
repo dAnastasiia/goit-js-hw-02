7d978b69f291e689c5d7ad04011ce235188601b0
DrAnastasiia <dr.anastasiyya0@gmail.com>
2020-09-30 12:15:59 +0300

    Add files for tasks

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const getItemsString = function (array) {
+  "use strict";
+  // Write code under this line
+
+  let result = "";
+
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+
+  return result;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
